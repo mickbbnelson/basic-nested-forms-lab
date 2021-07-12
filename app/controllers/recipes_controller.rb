94b61d02449c606c1aa8ddefc536131b072be59a
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-07-12 09:53:58 -0600

    commit tree 9566c5e624137717453b7bb3bea48c505361e17d

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,23 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(recipe_id: "1")
+    @recipe.ingredients.build(recipe_id: "2")
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
   end
+
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
+  end 
 end
